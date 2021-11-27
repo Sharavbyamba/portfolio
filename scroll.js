c92ffc4a4d89886a6288915e3355cc8650581e69
Sharavbyamba <90950351+Sharavbyamba@users.noreply.github.com>
2021-11-27 11:42:59 +0800

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,31 @@
+ScrollReveal().reveal('#dis', {
+    origin: 'top',
+    distance: '200px',
+    duration: 300,
+    scale: .2
+})
+ScrollReveal().reveal('#service', {
+    origin: 'right', 
+    distance: '200px',
+    duration: 1000
+})
+ScrollReveal().reveal('#aboutme', {
+    origin: 'left', 
+    distance: '200px',
+    duration: 1000
+})
+ScrollReveal().reveal('#lstyle', {
+    origin: 'bottom', 
+    distance: '200px',
+    duration: 1400
+})
+ScrollReveal().reveal('#contact', {
+    origin: 'bottom', 
+    distance: '200px',
+    duration: 1300
+})
+ScrollReveal().reveal('.footer', {
+    origin: 'bottom', 
+    distance: '200px',
+    duration: 1400
+})
